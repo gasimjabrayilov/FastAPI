fe47515e793c6bba2f5d1768f83eb75a5e7f30c1
Gasim Jabrayilov <gasims.jabrayilov@gmail.com>
2026-01-29 22:57:08 +0200

    postgre link adjusted

--- database.py
+++ database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:0000@127.0.0.1:3306/todoapplicationdatabase'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:KhCnBptVm9NAkPG5@db.epxhvtexuhopitruwszi.supabase.co:5432/postgres'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:KhCnBptVm9NAkPG5@db.epxhvtexuhopitruwszi.supabase.co:5432/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.epxhvtexuhopitruwszi:KhCnBptVm9NAkPG5@aws-1-eu-west-1.pooler.supabase.com:6543/postgres'
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
